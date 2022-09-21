644bc28d2446c4b6cc3498798ab30e32e365fd2c
Gonzalo <goznalomail@gmail.com>
2022-09-21 16:49:50 +0200

    Real and synthetic networks and their statistics

--- synthetic_networks.py
+++ synthetic_networks.py
@@ -3,7 +3,8 @@ Author: Gonzalo Contreras Aso
 Last modification: 21/09/2022
 Summary: Computation of the maximum alpha for PageRank full control, 
          maximum alpha for PageRank ranking control, and
-         maximum beta for biplex PageRank ranking control.
+         maximum beta for biplex PageRank ranking control
+         for synthetic networks (random and scale-free).
 """
 
 # Import standard network and data-science libraries
@@ -11,7 +12,7 @@ import numpy as np
 import networkx as nx
 import igraph as ig
 import pandas as pd
-import os
+from itertools import product
 from sklearn.preprocessing import normalize
 from scipy.sparse import csr_matrix, csc_matrix
 from scipy.sparse.linalg import inv as sparse_inv
@@ -47,44 +48,40 @@ def everything_together(G, sample=100, tol=1e-03, incr=0.01, power=3):
 
 network_stats = {}
 
-## Directed, weighted networks ##
-print("## Directed, weighted ##")
+# Parameters of the synthetic networks generated
+nodes = range(10, 20000, 1000)
+probs = [0.3, 0.5]
+sample = 10
 
-pathDW = 'Datasets/DW/'
-filesDW = os.listdir(pathDW)
-filesDW.sort()
-
-# Go file by file, loading their network and using above function 
-for file in filesDW:
-    name = file.split('.graphml')[0]
-    print('Processing', name)
-    G = ig.read(pathDW + file, format='graphml')
+## Random directed networks ##
+print("## Random directed networks ##")
+for n, p, s in product(nodes, probs, range(sample)):
+    if s == 0:
+        print(n, p)
+    G = ig.Graph.Erdos_Renyi(n, p*10/n, directed=True)
     props, alphas = everything_together(G)
 
-    network_stats[name] = list(props) + list(alphas)
+    network_stats[f'Random_{n}_{p*10/n}_{s}'] = list(props) + list(alphas)
 
 print()
 
-## Directed, unweighted networks ##
-print("## Directed, unweighted ##")
-
-pathDU = 'Datasets/DU/'
-filesDU = os.listdir(pathDU)
-filesDU.sort()
-
-# Go file by file, loading their network and using above function 
-for file in filesDU:
-    name = file.split('.graphml')[0]
-    print('Processing', name)
-    G = ig.read(pathDU + file, format='graphml')
+## Directed scale-free networks ##
+# In this case we need to first generate them with networkx, 
+# as igraph does not have an implementation for directed ones.
+print("## Directed scale-free networks ##")
+for n, p, s in product(nodes, probs, range(sample)):
+    if s == 0:
+        print(n, p)
+    Gx = nx.scale_free_graph(1000, alpha=p-0.05, beta=1-p, gamma=0.05)
+    Gx = nx.DiGraph(Gx)
+    G = ig.Graph.from_networkx(Gx)
     props, alphas = everything_together(G)
 
-    network_stats[name] = list(props) + list(alphas)
+    network_stats[f'ScaleFree_{n}_{p}_{s}'] = list(props) + list(alphas)
 
 
 ## Save to csv with appropriate columns ##
-
-df = pd.DataFrame.from_dict(network_stats, orient='index', columns = ['N', 'E', 'Directed?', 'Weighted?', 'alpha_f_PR', 'alpha_r_PR', 'alpha_r_BPR'])
-df.round(6).to_csv('NetworkStats/real_statistics.csv')
-
-
+df = pd.DataFrame.from_dict(network_stats, orient='index', columns = ['N', 'E', 'Directed?', 'Weighted?', 'alpha_f_PR', 'alpha_r_PR', 'beta_r_BPR'])
+net_type = [name.split('_')[0] for name in df.index]
+df['Type'] = net_type
+df.round(6).to_csv('NetworkStats/synthetic_statistics.csv')
